5e6ea3fb855c39629d07e71ac13e4b6343f59c37
Alejandro Cardenas <ogopogo@fibertel.com.ar>
2023-02-28 20:50:00 -0300

    Se agregan las categorias y card

---
+++ js/categories.js
@@ -0,0 +1,12 @@
+const dataCategory =  jsonData.events;
+const radio = document.getElementById('radio');
+let radioTemplate = "";
+
+const categoriesNoRepeat = [... new Set(dataCategory.map(e=>e.category))];
+
+categoriesNoRepeat.forEach((element, i) => {
+    radioTemplate +=`<label class="radio-color-label" for="category${i}"><input type="radio" name="category" id="category${i}" value="category">${element}</label>`
+
+    radio.innerHTML = radioTemplate;
+
+});
